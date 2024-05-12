17a5a20bf889974c408e29676540d3aebfea2a74
satyam <livester044@gmail.com>
2024-05-12 18:26:01 +0530

    flip effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import worldmap from '../public/worldmap.webp'
-import chippng from '../public/card-chip-800x450.png'
-import visapng from '../public/Visa-Symbol.webp'
+import React from 'react';
+import './App.css';
+import worldmap from '../public/worldmap.webp';
+import chippng from '../public/card-chip-800x450.png';
+import visapng from '../public/Visa-Symbol.webp';
+import pattern from '../public/pattern.png';
 
 const App = () => {
   return (
     <div className='h-screen w-screen bg-[#0a1e30] flex items-center justify-center'>
       <div className="container h-[80%] w-[80%] flex">
         <div className="c-left h-[100%] w-[25%] bg-gradient-to-tr from-[#902ae0] via-[#cb1eb5] to-[#e417a1] rounded-tl-2xl rounded-bl-2xl relative flex items-center">
-          <div className='h-52 w-80 absolute left-[30%] flex flex-col justify-between items-center z-10'>
-            <img src={worldmap} className='absolute -z-10 rounded-2xl brightness-50' />
-            <div className="absolute h-[103%] w-[100%] rounded-2xl bg-gradient-to-r from-[#821fce] via-[#1e9acb] to-[#e417a1] -z-10 opacity-50"></div>
-            <div className="top w-[90%] flex justify-between items-center">
-              <img src={chippng} className='w-12 h-12 object-cover' />
-              <img src={visapng} className='w-20 h-20 object-contain relative ' />
-            </div>
-            <div className="middle w-[90%] relative bottom-2 flex justify-center">
-              <input type="text" value="####   ####   ####   ####" className='w-[100%] bg-transparent text-white text-2xl pointer-events-none' />
-            </div>
-            <div className="bottom w-[90%] relative bottom-2 text-white flex justify-between">
-              <div className="cardholder">
-                <p className='text-xs'>Card Holder</p>
-                <p>Full Name</p>
+          <div className="card">
+            <div className='card-inner h-52 w-80 absolute left-[30%] rounded-2xl overflow-hidden z-10'>
+              <div className="front h-[100%] w-[100%] absolute flex flex-col justify-between items-center">
+                <img src={worldmap} className='absolute -z-10 brightness-50' />
+                <div className="absolute h-[100%] w-[100%] bg-gradient-to-r from-[#821fce] via-[#1e9acb] to-[#e417a1] -z-10 opacity-50"></div>
+                <div className="top w-[90%] flex justify-between items-center">
+                  <img src={chippng} className='w-12 h-12 object-cover' alt="Chip" />
+                  <img src={visapng} className='w-20 h-20 object-contain relative' alt="Visa" />
+                </div>
+                <div className="middle w-[90%] relative bottom-4 flex justify-center">
+                  <input type="text" value="####   ####   ####   ####" className='w-[100%] bg-transparent text-white text-2xl pointer-events-none' />
+                </div>
+                <div className="bottom w-[90%] relative bottom-4 text-white flex justify-between">
+                  <div className="cardholder">
+                    <p className='text-xs'>Card Holder</p>
+                    <p>Full Name</p>
+                  </div>
+                  <div className="expires">
+                    <p className='text-xs'>Expires on</p>
+                    <p>MM / YY</p>
+                  </div>
+                </div>
               </div>
-              <div className="expires">
-                <p className='text-xs'>Expires on</p>
-                <p>MM / YY</p>
+              <div className="back h-[100%] w-[100%] absolute flex flex-col justify-between items-center">
+                <img src={worldmap} className='absolute brightness-50' />
+                <div className="absolute h-[100%] w-[100%] bg-gradient-to-r from-[#821fce] via-[#1e9acb] to-[#e417a1] opacity-50"></div>
+                <div className="top w-[100%] h-9 relative top-5 bg-[#0000008c] z-10"></div>
+                <div className="middle w-[100%] flex justify-center items-center z-10">
+                  <img src={pattern} className='w-[80%]' alt="Pattern" />
+                  <input type="text" value="***" className='text-black pointer-events-none w-[10%]' />
+                </div>
+                <div className="bottom w-[100%] h-[10%] relative bottom-2 flex justify-end z-10">
+                  <img src={visapng} className='w-[30%] h-4 object-contain' alt="Visa" />
+                </div>
               </div>
             </div>
           </div>
@@ -33,7 +51,7 @@ const App = () => {
         <div className="c-right h-[100%] w-[75%] bg-[#ece6e6] rounded-tr-2xl rounded-br-2xl"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
